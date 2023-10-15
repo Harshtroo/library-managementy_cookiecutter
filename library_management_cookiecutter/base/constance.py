ce83860fb89b235104830c5e749c12d3a66bda4a
Harshtroo <123172610+Harshtroo@users.noreply.github.com>
2023-10-15 23:50:08 +0530

    Add email/group (#2)
    * initialization
    
    * Add app library (#1)
    
    * add sql in settings
    
    * add settings
    
    * chaneg seetings file
    
    * changed settings
    
    * add_library_app
    
    * add group and send email successfully
    
    * add_email/group
    
    * solve all commit
    
    * add pre-commit
    
    * solve base html
    
    * change login

---
+++ library_management_cookiecutter/base/constance.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Role(Enum):
+    student = "student"
+    librarian = "librarian"
+
+    @classmethod
+    def choices(cls):
+        return tuple((role.name, role.value) for role in cls)
